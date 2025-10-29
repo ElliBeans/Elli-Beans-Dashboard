7f0904f19902b12b5c96c78bdd4f6af0b0569b85
ElliBeans <elliscoffeethru@gmail.com>
2025-10-29 16:24:50 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>☕ Elli Beans Dashboard</h1>
+      <p>Connected and ready to build.</p>
+    </main>
+  );
+}
